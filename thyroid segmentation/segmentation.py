73a560921eb9f3b742a8e4b26d33402f7e3ff00a
Azest-japan <56631844+Azest-japan@users.noreply.github.com>
2020-04-07 16:15:27 +0900

    Update segmentation.py

--- thyroid segmentation/segmentation.py
+++ thyroid segmentation/segmentation.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 18 15:09:30 2019
+Created on Tue Mar 31 13:14:46 2020
 
 @author: AZEST-2019-07
 """
 
-
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import seaborn as sns
-from tensorflow import keras
+import os 
+import time
 import numpy as np
-import scipy
-import cv2
+import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.models import *
 from tensorflow.keras.metrics import *
 from tensorflow.keras.layers import *
@@ -24,10 +21,11 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import f1_score, confusion_matrix, precision_score, recall_score, jaccard_score
 from tensorflow.keras.callbacks import *
-from tensorflow.keras.applications import vgg16
+from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Cropping2D
 from tensorflow.keras.regularizers import l2
-import segmentation_models as sm
+import cv2
+
 
 #Reference libraries 
 #sklearn.model_selection -> Gridsearch, train_test_split
@@ -36,25 +34,36 @@ import segmentation_models as sm
 #sklearn.ensemble -> RandomForestClassifier, RandomForestRegressor,
 #sklearn.metrics -> max_error, mean_absolute_error, f1_score, jaccard_score, roc_curve, auc
 
-#keras.models -> model_from_json, model_from_yaml, save_model, Input, Sequential, Model
+#keras.models -> model_from_json, model_from_yaml, load_model, save_model, Input, Sequential, Model
 #keras.metrics -> accuracy, binary_accuracy, categorical_accuracy, sparse_categorical_accuracy
-#keras.layers -> Dense, Maxpooling2D, Dropout, Conv2D, concatenate, Conv2DTranspose...
+#keras.layers -> Dense, Cropping2D, Maxpooling2D, Dropout, Conv2D, concatenate, Conv2DTranspose...
 
 
-#loads data and splits it into test and train
-def load(fpath):
+input_shape = (320, 512, 1)
+n_filters = 32
+cf_size = (3, 3)   # convolution filter size
+mpf_size = (2, 2)  # max pooling size
+s_size = (2, 2)    # stride size
+batch_size = 32
+
+fpath = 'C:\\Users\\AZEST-2019-07\Desktop\\Ito\\Selected'
+fxml = 'C:\\Users\\AZEST-2019-07\Desktop\\Ito\\annotations\\final2.xml'
+
+
+def load(fpath,fxml):
+    xtrain,xtest,ytrain,ytest = None,None,None,None
     
-    (x_train, y_train), (x_test, y_test) = (None,None),(None,None)
-    return x_train,y_train,x_test,y_test
+    return xtrain,xtest,ytrain,ytest
 
-x_train,y_train,x_test,y_test = load()
+xtrain,xtest,ytrain,ytest = load(fpath,fxml)
 
 def save_model(model):    
     json_string = model.to_json()
     open('models.json', 'w').write(json_string)
     
-def load_model():
+def load_model2():
     model = model_from_json(open('models.json').read())
+    #model = load_model('C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\mymodel.h5')
     model.load_weights('C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\best_weightss.hdf5')
     opt = keras.optimizers.Adam(learning_rate=0.001)
     model.compile(loss=sm.losses.bce_jaccard_loss,
@@ -62,108 +71,100 @@ def load_model():
                   metrics=[sm.metrics.iou_score,f1])
     return model
 
-# self defined F1 metric
-def f1(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+def resunit(inp,nf=32,Ksize=3,padding='same',strides=1,BN='True',BN_first=True,activation='relu',sno='0'):
     
-    def recall(y_true, y_pred):
-        
-        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + K.epsilon())
-        return recall
-
-    def precision(y_true, y_pred):
-        
-        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-        precision = true_positives / (predicted_positives + K.epsilon())
-        return precision
-    
-    precision = precision(y_true, y_pred)
-    recall = recall(y_true, y_pred)
-    
-    return 2*((precision*recall)/(precision+recall+K.epsilon()))
-
-def resunit(inp,nf=32,Ksize=3,padding='same',strides=1,BN='True',BN_first=True,activation='relu'):
     
     conv = Conv2D(nf,
                   kernel_size=Ksize,
                   strides=strides,
                   padding=padding,
                   kernel_initializer='he_normal',
-                  kernel_regularizer=l2(1e-4))
+                  kernel_regularizer=l2(1e-4),
+                  name='C'+sno)
     
     x = inp
     
     if BN_first:
         if BN:
-            x = BatchNormalization()(x)
+            x = BatchNormalization(name='BN'+sno)(x)
         if activation is not None:
-            x = Activation(activation)(x)
+            x = Activation(activation,name='Act'+sno)(x)
         x = conv(x)
     else:
         x = conv(x)
         if BN:
-            x = BatchNormalization()(x)
+            x = BatchNormalization(name='BN'+sno)(x)
         if activation is not None:
-            x = Activation(activation)(x)
+            x = Activation(activation,name='Act'+sno)(x)
     return x
 
 
-def resnet(input_shape,num_classes=2,nf=32,nb=4):
+def resnet(input_shape,n_classes=2,nf=32,nb=4):
 
     Ksize=3
     padding='same'
     strides=1
-    
-    conv = Conv2D(nf,
-                  kernel_size=Ksize,
-                  strides=strides,
-                  padding=padding,
-                  kernel_initializer='he_normal')
+    ld = {}
+    nf = 32
     
     
     inputs = Input(input_shape)
-    x = resunit(inputs,nf=nf,Ksize=3,padding='same',strides=1,BN_first=False,activation='relu')
-    x = conv(x)
-    x = add([inputs, x])
-    
+    x = resunit(inputs,nf=nf,Ksize=3,padding='same',strides=1,BN_first=False,activation='relu',sno='00')
+    x = Conv2D(nf,kernel_size=3,strides=1,padding='same',kernel_initializer='he_normal', kernel_regularizer=l2(1e-4), name='C01')(x)
+    x = add([inputs, x],name='A0')
+    ld['A0'] = x
     for i in range(nb):
         nf = 2*nf
-        y = resunit(x,nf,strides=2)
-        y = resunit(y,nf)
-        x = Conv2D(nf,kernel_size=1,padding='same',strides=2)(x)
-        x = add([x,y])
+        y = resunit(x,nf,strides=2,sno=str((i+1)*10))
+        y = resunit(y,nf,sno=str((i+1)*10+1))
+        x = Conv2D(nf,kernel_size=1,padding='same',strides=2,name='1C'+str((i+1)*10))(x)
+        x = add([x,y],name='A0'+str(i+1))
+        ld['A'+str(i+1)] = x
     
-    x = AveragePooling2D(pool_size=2)(x)
-    y = Flatten()(x)
-    outputs = Dense(num_classes,
-                    activation='softmax',
-                    kernel_initializer='he_normal')(y)
-
+#    x1 = AveragePooling2D(pool_size=2)(x)
+#    y1 = Flatten()(x1)
+#    outputs = Dense(num_classes,
+#                    activation='softmax',
+#                    kernel_initializer='he_normal')(y1)
+        
+    for i in range(nb):
+        nf = int(nf/2)
+        if i<2:
+            x = Conv2DTranspose(nf, kernel_size=Ksize, strides=2, padding=padding, kernel_initializer='he_normal')(x)
+        else:
+            x = UpSampling2D((2,2))(x)
+        x = concatenate([ld['A'+str(nb-i-1)],x],axis=3,name='CT'+str(i))
+        y = resunit(x,nf,sno=str((i+5)*10))
+        y = resunit(y,nf,sno=str((i+5)*10+1))
+        x = Conv2D(nf,kernel_size=1,padding='same',strides=1,name='1C'+str((i+5)*10))(x)
+        x = add([x,y],name='A0'+str(i+5))
+    
+    
+    x = Conv2D(n_classes,kernel_size=1,padding='same',strides=1,name='out')(x)
     # Instantiate model.
-    model = Model(inputs=inputs, outputs=outputs)
+    model = Model(inputs=inputs, outputs=x)
     return model
 
 
+    
 def train():
-    tensorboard = TensorBoard(log_dir='C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\logs\\tb1')
+    #tensorboard = TensorBoard(log_dir='C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\logs\\tb1')
     
-    #model = sm.Unet('vgg16', input_shape=(448, 512, 3), classes = 5, activation='sigmoid',encoder_weights='imagenet')
     model = resnet(input_shape=input_shape)
-    #print(model.summary())
-    opt = keras.optimizers.Adam(learning_rate=0.001)
+    print(model.summary())
+    # opt = keras.optimizers.Adam(learning_rate=0.001)
     
-    loss=sm.losses.JaccardLoss()
-    model.compile(loss=loss,
-                      optimizer=opt,
-                      metrics=[sm.metrics.iou_score,f1])
+    # loss=sm.losses.JaccardLoss()
+    # model.compile(loss=loss,
+    #                   optimizer=opt,
+    #                   metrics=[sm.metrics.iou_score,f1])
     
-    checkpointer = ModelCheckpoint(filepath="C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\best_weights.hdf5", 
-                                   monitor = 'val_accuracy',
-                                   verbose=1, 
-                                   save_best_only=True)
+    # checkpointer = ModelCheckpoint(filepath="C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\best_weights.hdf5", 
+    #                                monitor = 'val_accuracy',
+    #                                verbose=1, 
+    #                                save_best_only=True)
 
 
+train()
 
-    
 
